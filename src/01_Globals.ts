7118bb5ffe939cd953dec2cbab25f527c2a2f0ab
HoweZae <jdtomanan@up.edu.ph>
2025-01-13 22:30:39 +0800

    fix: Add null to enum Column

--- src/01_Globals.ts
+++ src/01_Globals.ts
@@ -29,7 +29,7 @@ var incoming: IncomingSheet;
  */
 type RowNumber = number;
 enum Column {
-    A = 1,
+    null = 0, A = 1,
     B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
 }
 
